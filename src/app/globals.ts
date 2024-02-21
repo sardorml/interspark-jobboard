282963f874a7a5447813bc561eb88fa1bbabef19
sardorml <sardormamadaliyev@gmail.com>
2024-02-21 23:27:27 +0900

    added types and globals

---
+++ src/app/globals.ts
@@ -0,0 +1,34 @@
+import { Input } from './interfaces/input.type';
+
+export const FormFields: Input[] = [
+  {
+    name: 'jobNumber',
+    title: 'Job number',
+    placeholder: 'Enter job number',
+  },
+  {
+    name: 'title',
+    title: 'Title',
+    placeholder: 'Enter title',
+  },
+  {
+    name: 'startDate',
+    title: 'Start date',
+    placeholder: 'Enter start date',
+  },
+  {
+    name: 'closeDate',
+    title: 'Close date',
+    placeholder: 'Enter close date',
+  },
+  {
+    name: 'notes',
+    title: 'Notes',
+    placeholder: 'Enter notes',
+  },
+  {
+    name: 'numberOfOpenings',
+    title: 'Number of openings',
+    placeholder: 'Enter number of openings',
+  },
+];

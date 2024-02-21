5f06c0577d7a64ce57e71687df8e7ea14a7cc949
sardorml <sardormamadaliyev@gmail.com>
2024-02-21 23:27:39 +0900

    update app configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { JobDetailComponent } from './pages/job-detail/job-detail.component';
+import { NewJobComponent } from './pages/new-job/new-job.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'jobs', pathMatch: 'full' },
+  {
+    path: 'jobs',
+    component: JobsComponent,
+  },
+  {
+    path: 'jobs/new',
+    component: NewJobComponent,
+  },
+  {
+    path: 'jobs/:id',
+    component: JobDetailComponent,
+  },
+];
